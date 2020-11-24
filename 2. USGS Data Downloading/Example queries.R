65f28e356569aa8a47d1d62986c80c4f22044cd7
bolotinl <bolotinljb@gmail.com>
2020-11-24 10:37:45 -0800

    Redid concurrent overlap to get an idea of Western US sites

--- 2. USGS Data Downloading/Example queries.R
+++ 2. USGS Data Downloading/Example queries.R
@@ -2,7 +2,7 @@
 #Script for filtering data queries
 #Created 5/15/2020
 #===============================================================================
-setwd("~/Desktop/Blaszczak Lab/GB CO WQ Data/USGS Data Retrieval from Phil")
+setwd("/Volumes/Blaszczak Lab/FSS/All Data")
 library("here") # WORKING DIRECTORY MUST BE CORRECT BEFORE CALLING THIS PACKAGE AND USING IT
 library("rgdal") #For CRS
 library("data.table")
@@ -45,8 +45,6 @@ source(here("concurrent_overlap.R"))
   SC_lotic_sensor <- subset(SC_query, SC_query$Site_ID %in% SC_lotic_sensor$Site_ID)
   disch_lotic_sensor <- subset(disch_query, disch_query$Site_ID %in% disch_lotic_sensor$Site_ID)
   
-  setwd("/Volumes/Blaszczak Lab/FSS/All Data")
-  
   write.csv(SC_lotic_sensor, "USGS_all_SC_lotic_sensor.csv") # as opposed to SC_only which is sites that have SC and not disch
   write.csv(disch_lotic_sensor,"USGS_all_disch_lotic_sensor.csv") # as opposed to disch_only which is sites that have disch and not SC
   
@@ -56,36 +54,40 @@ source(here("concurrent_overlap.R"))
 #-------------------------------------------------
 #Filter for all lotic NWIS sites with specific conductivity and discharge sensors (subdaily or daily)  
 #-------------------------------------------------
-#   #Since the specific conductivity query contains two different parameter codes ("00094", "00095")
-#   #We have to do a little trick to get the overlap function to work by giving them all the same parameter code
-#     SC_query2 <- SC_query
-#     SC_query2$parm_cd <- "00000"
-# 
-#   #Find concurrent overlap of data
-#     disch_SC_sensor <- overlap_fun(
-#       param_queries = list(disch_query, SC_query2), 
-#       site_types = c("ST", "ST-CA", "ST-DCH", "ST-TS", "SP"), 
-#       data_types = c("uv", "dv"), 
-#       min_obs = 1
-#     )  # there are 2002 sites with flowing water in the COUNTRY with SC and Discharge sensors  
-#     
-# write.csv(disch_SC_sensor, "disch_SC_sensor.csv") # Flowing waters with Q and SC
+  #Since the specific conductivity query contains two different parameter codes ("00094", "00095")
+  #We have to do a little trick to get the overlap function to work by giving them all the same parameter code
+    SC_query2 <- SC_query
+    SC_query2$parm_cd <- "00000"
 
+  #Find concurrent overlap of data
+    disch_SC_sensor <- overlap_fun(
+      param_queries = list(disch_query, SC_query2),
+      site_types = c("ST", "ST-CA", "ST-DCH", "ST-TS", "SP"),
+      data_types = c("uv", "dv"),
+      min_obs = 1
+    )  # there are 2002 sites with flowing water in the COUNTRY with SC and Discharge sensors
 
-# #See what sites have SC that do not have discharge    
+    # As it is, the output dataframe is just the SiteID and the Lat Long. Let's include huc_cd.
+    disch_SC_sensor <- disch_query[which(disch_query$Site_ID %in% disch_SC_sensor$Site_ID),]
+    names(disch_SC_sensor)
+    disch_SC_sensor <- select(disch_SC_sensor, c("Site_ID", "station_nm", "Lat", "Lon", "huc_cd"))
+    disch_SC_sensor <- unique(disch_SC_sensor)
+write.csv(disch_SC_sensor, "USGS_all_disch_SC_lotic_sensor.csv") # Flowing waters with Q and SC
+
+# #See what sites have SC that do not have discharge
 # SC_only <- setdiff(SC_lotic_sensor, disch_SC_sensor) # 811 sites
 # unique(SC_query$Site_ID)
 # SC_query[duplicated(SC_query$Site_ID),]
 # SC_only <- SC_query[which(SC_query$Site_ID %in% SC_only$Site_ID),]
-# # SC_only$Site_ID <- as.factor(SC_only$Site_ID)  
-# # levels(SC_only$Site_ID) 
-# # SC_only$site_tp_cd <- as.factor(SC_only$site_tp_cd)
-# # levels(SC_only$site_tp_cd) # already filtered for only flowing waters
-# 
+# SC_only$Site_ID <- as.factor(SC_only$Site_ID)
+# levels(SC_only$Site_ID)
+# SC_only$site_tp_cd <- as.factor(SC_only$site_tp_cd)
+# levels(SC_only$site_tp_cd) # already filtered for only flowing waters
+
 # write.csv(SC_only, "SC_sensor_only.csv")
-# 
+#
 # #See what sites have discharge that do not have SC
 # disch_only <- setdiff(disch_lotic_sensor, disch_SC_sensor)
 # disch_only <- disch_query[which(disch_query$Site_ID %in% disch_only$Site_ID),]
-# 
+#
 # write.csv(disch_only, "disch_sensor_only.csv")
