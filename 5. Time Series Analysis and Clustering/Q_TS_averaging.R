7858ed31974d2d1e55566aff645832b1040b64e1
bolotinl <bolotinljb@gmail.com>
2020-12-11 15:33:54 -0800

    Made flow figures for WUS sites

--- 5. Time Series Analysis and Clustering/Q_TS_averaging.R
+++ 5. Time Series Analysis and Clustering/Q_TS_averaging.R
@@ -9,8 +9,10 @@ theme_set(theme(legend.position = "none",panel.background = element_blank(),
                 axis.line = element_line(colour = "black")))
 
 setwd("/Volumes/Blaszczak Lab/FSS/All Data")
-dat <- readRDS("USGS_disch_dqi.rds")
-sites <- readRDS("TS_avg_site_selection.rds")
+# dat <- readRDS("USGS_disch_dqi.rds")
+# sites <- readRDS("TS_avg_site_selection.rds")
+dat <- readRDS("WUS_USGS_SC_Q_availability_subset.rds")
+
 
 ## Format dataframe and add important details
 dat$Date <- as.POSIXct(as.character(dat$Date), format = "%Y-%m-%d")
@@ -52,10 +54,19 @@ low_quart <- low_quart %>%
   group_by(SiteID, doy) %>%
   summarise_at(.vars = "Q_cms", .funs = c("lower_quart" = quant25))
 
+p2 <- ggplot(subset(dat, dat$SiteID == "USGS-10133800"))+
+  geom_line(mapping = aes(x = doy, y = Q_cms, color = Year))+
+  geom_line(subset(low_quart, up_quart$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = lower_quart), color = "black")+
+  geom_line(subset(med, avg$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = median), color = "black")+
+  geom_line(subset(up_quart, up_quart$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = upper_quart), color = "black")+
+  geom_line(subset(avg, avg$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = mean), color = "red")
+print(p2)
+
+
 ## Rerun Code to Make PDF's of All Plots of All Data + Quantile + Mean
 setwd("/Volumes/Blaszczak Lab/FSS/Figures/SingleTSPlots")
 plotSpC <- function(x){
-  pdf(paste0(x, "_singleTS_flow_only.pdf"))
+  pdf(paste0("WUS_", x, "_singleTS_flow_only.pdf"))
   p <- ggplot(subset(dat, dat$SiteID == x))+
     theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"))+
     geom_line(mapping = aes(x = doy, y = Q_cms, color = Year))+
@@ -67,12 +78,14 @@ plotSpC <- function(x){
   dev.off()
 }
 
+dat$SiteID <- factor(dat$SiteID)
+sites <- levels(dat$SiteID)
+
 ## Test for one site
 # x <- "USGS-09014050"
 # plotSpC(x)
 # rm(x)
-sites$SiteID <- as.factor(sites$SiteID)
-sites <- levels(sites$SiteID) 
+
 ## Apply to all sites
 lapply(sites, plotSpC) # already ran and saved PDFs
 
