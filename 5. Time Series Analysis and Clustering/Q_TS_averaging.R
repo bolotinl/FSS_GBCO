fb8fcb6b7258e440e92364147bd58362bbdc5f42
bolotinl <bolotinljb@gmail.com>
2021-02-19 10:06:52 -0800

    Finalized comments

--- 5. Time Series Analysis and Clustering/Q_TS_averaging.R
+++ 5. Time Series Analysis and Clustering/Q_TS_averaging.R
@@ -1,3 +1,6 @@
+# Create annual time series representative of each site across years 
+# This script: discharge data only
+
 ## Bring in packages
 rm(list=ls())
 x <- c("tidyverse", "data.table", "lubridate", "zoo")
@@ -9,18 +12,19 @@ theme_set(theme(legend.position = "none",panel.background = element_blank(),
                 axis.line = element_line(colour = "black")))
 
 setwd("/Volumes/Blaszczak Lab/FSS/All Data")
-# dat <- readRDS("USGS_disch_dqi.rds")
-# sites <- readRDS("TS_avg_site_selection.rds")
-dat <- readRDS("WUS_USGS_SC_Q_availability_subset.rds")
+dat <- readRDS("WUS_UNM_USGS_SC_Q_availability_subset.rds")
 
 
 ## Format dataframe and add important details
 dat$Date <- as.POSIXct(as.character(dat$Date), format = "%Y-%m-%d")
 dat$Year <- year(dat$Date) 
 dat$Year <- as.factor(dat$Year)
+# Create column with the Julian Date (day of year or "doy")
 dat$doy <- strftime(dat$Date, format = "%j")
 dat$doy <- as.numeric(as.character(dat$doy))
 dat <- mutate(dat, SiteYear = paste(SiteID, Year, sep = " "))
+# Get rid of unused factor levels for SiteID
+dat$SiteID <- factor(dat$SiteID) # 246 sites
 
 ## MEAN #########
 avg <- dat
@@ -38,7 +42,7 @@ up_quart <- up_quart %>%
   group_by(SiteID, doy) %>%
   summarise_at(.vars = "Q_cms", .funs = c("upper_quart" = quant75))
 
-## Median (Middle Quatile)
+## Median (Middle Quantile)
 med <- dat
 med <- med %>%
   group_by(SiteID, doy) %>%
@@ -66,7 +70,7 @@ print(p2)
 ## Rerun Code to Make PDF's of All Plots of All Data + Quantile + Mean
 setwd("/Volumes/Blaszczak Lab/FSS/Figures/SingleTSPlots")
 plotSpC <- function(x){
-  pdf(paste0("WUS_", x, "_singleTS_flow_only.pdf"))
+  pdf(paste0("WUS_UNM_", x, "_singleTS_flow_only.pdf"))
   p <- ggplot(subset(dat, dat$SiteID == x))+
     theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"))+
     geom_line(mapping = aes(x = doy, y = Q_cms, color = Year))+
@@ -89,17 +93,15 @@ sites <- levels(dat$SiteID)
 ## Apply to all sites
 lapply(sites, plotSpC) # already ran and saved PDFs
 
-## Plot example:
-p3 <- ggplot(subset(dat, dat$SiteID == "USGS-10133800"))+
-  geom_line(mapping = aes(x = doy, y = Q_cms, color = Year))+
-  geom_line(subset(low_quart, up_quart$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = lower_quart), color = "black")+
-  geom_line(subset(med, avg$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = median), color = "black")+
-  geom_line(subset(up_quart, up_quart$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = upper_quart), color = "black")+
-  geom_line(subset(avg, avg$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = mean), color = "red")
-print(p3)
-
 ## Save data files
-saveRDS(low_quart, "Q_low_quart.rds")
-saveRDS(avg, "Q_avg.rds")
-saveRDS(up_quart, "Q_up_quart.rds")
-saveRDS(med, "Q_med.rds")
+saveRDS(low_quart, "WUS_UNM_Q_low_quart.rds")
+write.csv(low_quart, "WUS_UNM_Q_low_quart.csv")
+
+saveRDS(avg, "WUS_UNM_Q_avg.rds")
+write.csv(avg, "WUS_UNM_Q_avg.csv")
+
+saveRDS(up_quart, "WUS_UNM_Q_up_quart.rds")
+write.csv(up_quart, "WUS_UNM_Q_up_quart.csv")
+
+saveRDS(med, "WUS_UNM_Q_med.rds")
+write.csv(med, "WUS_UNM_Q_med.csv")
