ee5d0698d3a5c0b616d1ef574afcc4b93bde2c96
bolotinl <bolotinljb@gmail.com>
2021-02-18 17:32:07 -0800

    Finalized comments

--- 5. Time Series Analysis and Clustering/SC_Q_TS_averaging.R
+++ 5. Time Series Analysis and Clustering/SC_Q_TS_averaging.R
@@ -1,3 +1,6 @@
+# Create representative annual time series for each site
+# This script: Flow-normalized SC data
+
 rm(list=ls())
 x <- c("tidyverse", "data.table", "lubridate", "zoo")
 lapply(x, require, character.only = TRUE)
@@ -7,32 +10,22 @@ rm(x)
 theme_set(theme(legend.position = "none",panel.background = element_blank(), 
                 axis.line = element_line(colour = "black")))
 
+## Bring in all specific conductance data (flow-normalized)
 setwd("/Volumes/Blaszczak Lab/FSS/All Data")
-# dat <- readRDS("all_SC_Q_data.rds")
-# sites <- readRDS("TS_avg_site_selection.rds")
-# sites$SiteID <- as.factor(sites$SiteID)
-# sites <- levels(sites$SiteID)
-# dat <- subset(dat, dat$SiteID %in% sites) # 407,672
-# dat <- readRDS("WUS_USGS_SC_Q_availability_subset.rds")
 dat <- readRDS("WUS_UNM_USGS_SC_Q_availability_subset.rds")
 
-
-## Add some important details
+## Add some important details and format dataframe
 dat$Date <- as.POSIXct(as.character(dat$Date), format = "%Y-%m-%d")
 dat$Year <- year(dat$Date) 
 dat$Year <- as.factor(dat$Year)
+# Assign Julian Date (day of year or doy) to each observation
 dat$doy <- strftime(dat$Date, format = "%j")
 dat$doy <- as.numeric(as.character(dat$doy))
-dat <- select(dat, -c("SiteDate"))
 dat <- mutate(dat, SiteYear = paste(SiteID, Year, sep = " "))
-
 dat$SpC_Qcms <- as.numeric(as.character(dat$SpC_Qcms))
+dat$SiteID <- factor(dat$SiteID) # Make sure we get rid of factor levels for SiteIDs we aren't using
 
-# is.nan.data.frame <- function(x)
-#   do.call(cbind, lapply(x, is.nan))
-# 
-# dat[is.nan(dat)] <- NA
-## Rerun Quantile and Mean Code for Filtered Dataset ####
+## Create Representative Annual Time Series - MEAN ############################################################################################################
 ## Mean
 avg <- dat
 avg$SpC_Qcms <- as.numeric(as.character(avg$SpC_Qcms))
@@ -41,9 +34,9 @@ avg <- avg %>%
   summarise_at(.vars = "SpC_Qcms", .funs = c("mean" = mean))
 
 ## Check that this worked 
-# check <- subset(dat, dat$SiteID == "USGS-09014050" & dat$doy == 1)
-# check_avg <- mean(check$SpC)
-# rm(check, check_avg) # all good
+check <- subset(dat, dat$SiteID == "USGS-06036905" & dat$doy == 1)
+check_avg <- mean(check$SpC_Qcms)
+rm(check, check_avg) # all good
 
 ## Upper Quantile
 quant75 <- function(x){
@@ -71,6 +64,7 @@ low_quart <- low_quart %>%
   group_by(SiteID, doy) %>%
   summarise_at(.vars = "SpC_Qcms", .funs = c("lower_quart" = quant25))
 
+# Example plot
 p2 <- ggplot(subset(dat, dat$SiteID == "USGS-10133800"))+
   geom_line(mapping = aes(x = doy, y = SpC_Qcms, color = Year))+
   geom_line(subset(low_quart, up_quart$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = lower_quart), color = "black")+
@@ -78,18 +72,19 @@ p2 <- ggplot(subset(dat, dat$SiteID == "USGS-10133800"))+
   geom_line(subset(up_quart, up_quart$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = upper_quart), color = "black")+
   geom_line(subset(avg, avg$SiteID == "USGS-10133800"), mapping = aes(x = doy, y = mean), color = "red")
 print(p2)
-# saveRDS(low_quart, "SC_Q_low_quart.rds")
-# saveRDS(avg, "SC_Q_avg.rds")
-# saveRDS(up_quart, "SC_Q_up_quart.rds")
-# saveRDS(med, "SC_Q_med.rds")
-# saveRDS(low_quart, "WUS_SC_Q_low_quart.rds")
-# saveRDS(avg, "WUS_SC_Q_avg.rds")
-# saveRDS(up_quart, "WUS_SC_Q_up_quart.rds")
-# saveRDS(med, "WUS_SC_Q_med.rds")
+
+# Save dataframes with the averaged time series we created
 saveRDS(low_quart, "WUS_UNM_SC_Q_low_quart.rds")
-saveRDS(avg, "WUS_UNM_SC_Q_avg.rds")
+write.csv(low_quart, "WUS_UNM_SC_Q_low_quart.csv")
+
+saveRDS(avg, "WUS_UNM_SC_Q_avg.rds") # This is what we will use for the final clustering
+write.csv(avg, "WUS_UNM_SC_Q_avg.csv")
+
 saveRDS(up_quart, "WUS_UNM_SC_Q_up_quart.rds")
+write.csv(up_quart, "WUS_UNM_SC_Q_up_quart.csv")
+
 saveRDS(med, "WUS_UNM_SC_Q_med.rds")
+write.csv(med, "WUS_UNM_SC_Q_med.csv")
 
 ## Function to create TS plots for all sites
 setwd("/Volumes/Blaszczak Lab/FSS/Figures/SingleTSPlots")
