7fc08b631f5759f1fc2df15d83d607f4e9553171
bolotinl <bolotinljb@gmail.com>
2021-03-01 15:23:45 -0800

    Update RF_data_prep.R

--- 6. Catchment Attribute Analysis/RF_data_prep.R
+++ 6. Catchment Attribute Analysis/RF_data_prep.R
@@ -28,6 +28,7 @@ saveRDS(cor_high, "corr_high_all_attributes.rds")
 cor_high$Var1 <- factor(cor_high$Var1)
 (cor_remove <- levels(cor_high$Var1)) # Isolate the individual attributes that are highly correlated with at least one other attribute
 
+# Filter by correlation ####
 # Remove attributes according to Joanna's recommendations and other further investigations of correlation matrices and hypotheses for attribute impacts on salinity
 names(dat)
 dat <- dat %>%
@@ -67,6 +68,8 @@ cor_high <-
   dplyr::distinct() %>% 
   dplyr::arrange(Var1, Var2)
 
+# Group Land Use ####
+
 # All of the developed land uses are correlated and could potentially be combined
 # Group ALL land use classes: 
 # Developed
@@ -96,6 +99,95 @@ dat <- dat %>%
   select(-c(WBM_SM_STRG, OM_Content))
 saveRDS(dat, "attribute_tune_df_3.rds")
 
+# Add road density ####
+setwd("/Volumes/Blaszczak Lab/FSS/All Data")
+dat <- readRDS("attribute_df_cluster_results.rds")
+roads_list <- list.files(path = "/Volumes/Blaszczak Lab/StreamCat Dataset", pattern = "RoadDensity")
+roads_list
+
+setwd("/Volumes/Blaszczak Lab/StreamCat Dataset")
+roads <- lapply(roads_list, read.csv)
+roads <- bind_rows(roads)
+roads <- roads %>%
+  select(c(COMID, RdDensWs))
+dat <- merge(dat, roads, by = "COMID", all.x = TRUE)
+
+setwd("/Volumes/Blaszczak Lab/FSS/All Data")
+dat_sub_by <- readRDS("attribute_tune_df_2.rds")
+
+dat_sub_by$RdDensWS <- dat$RdDensWs
+saveRDS(dat_sub_by, "attribute_tune_df_4.rds")
+
+# Add population density 2010 ####
+pop <- read.csv("/Volumes/Blaszczak Lab/FSS/NHD Attributes/Population Infrastructure/POPDENS10_CONUS.csv")
+pop <- pop %>% select(c(COMID, ACC_POPDENS10))
+dat <- readRDS("attribute_df_cluster_results.rds")
+
+dat <- merge(dat, pop, by = "COMID", all.x = TRUE)
+
+dat_sub_by <- readRDS("attribute_tune_df_2.rds")
+dat_sub_by$PopDens2010 <- dat$ACC_POPDENS10
+saveRDS(dat_sub_by, "attribute_tune_df_5.rds")
 
 
 
+
+
+
+
+
+
+
+
+
+
+# library(Boruta)
+# library(caret)
+# library(mlbench)
+# library(randomForest)
+# data("Sonar")
+# str(Sonar)
+# 
+# set.seed(111)
+# boruta <- Boruta(Class ~., 
+#                  data = Sonar, 
+#                  doTrace = 2, # Allows you to see what's going on in the console
+#                  maxRuns = 500)
+# print(boruta)
+# plot(boruta, 
+#      las = 2, # Makes axis lables vertical so you can see them all
+#      cex.axis = 0,7) # Changes font size on x axis
+# # Blue boxplots are shadow attributes
+# # Green boxplots are confirmed important
+# # Red boxplots confirmed to be not important
+# # Yellow boxplots inconclusive importance
+# plotImpHistory(boruta)
+# 
+# # Tentative Fix
+# # come up with a quick decision for tentative attributes
+# bor <- TentativeRoughFix(boruta)
+# bor # Now all attributes are classified as important or not important
+# attStats(boruta)
+# # normHits example, if 80% for a variable, 80% of the time, this variable was more important than the shadow attribute
+# 
+# # Then go through RF model
+# # Try normal model with all variables
+# 
+# # Work on removing variables confirmed not important by boruta
+# getNonRejectedFormula(boruta)
+# # 41 attributes (confirmed, plus tentative with quick fix)
+# # paste formula into new RF model
+# rf41 <- randomForest(Class ~ V1 + V2 + V4 + V5 + V8 + V9 + V10 + V11 + V12 + V13 + 
+#                        V14 + V15 + V16 + V17 + V18 + V19 + V20 + V21 + V22 + V23 + 
+#                        V26 + V27 + V28 + V30 + V31 + V32 + V35 + V36 + V37 + V39 + 
+#                        V43 + V44 + V45 + V46 + V47 + V48 + V49 + V51 + V52 + V54,
+#                      data = train)
+# # SLIGHTLY less accuracy after chopping ~1/3 attributes
+# 
+# getConfirmedFormula(boruta)
+# rf33 <- randomForest(Class ~ V1 + V4 + V5 + V9 + V10 + V11 + V12 + V13 + V15 + V16 + 
+#                        V17 + V18 + V19 + V20 + V21 + V22 + V23 + V26 + V27 + V28 + 
+#                        V31 + V35 + V36 + V37 + V43 + V44 + V45 + V46 + V47 + V48 + 
+#                        V49 + V51 + V52,
+#                      data = train)
+# # SIMILAR to original accuracy, but with ~1/2 the attributes
